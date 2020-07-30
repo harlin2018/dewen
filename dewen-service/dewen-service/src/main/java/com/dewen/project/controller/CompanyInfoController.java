fb2732214aa4734c8ed21bef35e4386cbdf8c4ef
huanghl <hhl@pccw-hengqin.com>
2020-07-30 18:25:48 +0800

    sss

--- dewen-service/dewen-service/src/main/java/com/dewen/project/controller/CompanyInfoController.java
+++ dewen-service/dewen-service/src/main/java/com/dewen/project/controller/CompanyInfoController.java
@@ -219,7 +219,8 @@ public class CompanyInfoController extends BaseController{
     }
     @RequestMapping(value = "/download/exportExcel", method = RequestMethod.POST)
     public BaseResponse exportExcel(@RequestBody ExportParam exportParam, HttpServletRequest request) throws Exception {
-        String path = ResourceUtils.getURL("classpath:").getPath();
+        // String path = ResourceUtils.getURL("classpath:").getPath();
+        String path = "C:/dewen/down";
         try {
             List<String> fieIds = exportParam.getFieIds();
             List<Integer> ids = exportParam.getIds();
